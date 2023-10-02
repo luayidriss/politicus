e1a677dce9429766ba4f86826885157ee9abf363
luayidriss <idrissluay@gmail.com>
2023-10-02 11:41:54 +0000

    Fix indentation

--- politicus/settings.py
+++ politicus/settings.py
@@ -65,7 +65,7 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
- else:
+else:
      CORS_ALLOWED_ORIGIN_REGEXES = [
          r"^https://.*\.gitpod\.io$",
      ]

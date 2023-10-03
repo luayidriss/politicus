534d05c59b65acd685d648df8e4251546a516964
luayidriss <idrissluay@gmail.com>
2023-10-03 11:59:44 +0000

    Add Navigation Bar Component and Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/Home';
+import Questions from './pages/Questions';
+import Profiles from './pages/Profiles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/questions" component={Questions} />
+          <Route path="/user-profiles" component={Profiles} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Profiles.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Profiles = () => {
+  return <div>Profile Page</div>;
+};
+
+export default Profiles;

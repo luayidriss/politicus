09026b54ecd9dffbba3e882e4c5b6ca5c4f8885c
luayidriss <idrissluay@gmail.com>
2023-10-03 11:56:22 +0000

    Set up Navigation Bar Components and App Routes

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,27 @@
+// src/components/NavigationBar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const NavigationBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/questions">Questions</Link>
+        </li>
+        <li>
+          <Link to="/user-profiles">User Profiles</Link>
+        </li>
+      </ul>
+    </nav>
+    
+  );
+};
+
+export default NavigationBar;
---
+++ frontend/src/pages/Questions.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Questions = () => {
+  return <div>Questions Page</div>;
+};
+
+export default Questions;

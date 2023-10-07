1572e08154eb8c316cd4511e1429ab54b1ceece6
luayidriss <idrissluay@gmail.com>
2023-10-07 11:16:00 +0000

    Add Edit and Delete functions to response list

--- frontend/src/components/ResponseList.js
+++ frontend/src/components/ResponseList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function ResponseList({ questionId }) {
     const [responses, setResponses] = useState([]);
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
         axios.get(`/api/responses/?question=${questionId}`)
@@ -12,7 +13,45 @@ function ResponseList({ questionId }) {
             .catch((error) => {
                 console.error('Error fetching responses:', error);
             });
+        
+        axios.get('/api/current-user')
+            .then((response) => {
+                setUser(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching current user:', error);
+            });
     }, [questionId]);
+
+    const handleEdit = (responseId, newContent) => {
+        axios.put(`/api/responses/${responseId}`, { content: newContent })
+            .then(() => {
+                refreshResponses();
+            })
+            .catch((error) => {
+                console.error('Error editing response:', error);
+            });
+    };
+
+    const handleDelete = (responseId) => {
+        axios.delete(`/api/responses/${responseId}`)
+            .then(() => {
+                refreshResponses();
+            })
+            .catch((error) => {
+                console.error('Error deleting response:', error);
+            });
+    };
+
+    const refreshResponses = () => {
+        axios.get(`/api/responses/?question=${questionId}`)
+            .then((response) => {
+                setResponses(response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching responses:', error);
+            });
+    };
 
     return (
         <div>
@@ -22,6 +61,22 @@ function ResponseList({ questionId }) {
                     <li key={response.id}>
                         {response.content}
                         <p>Author: {response.user.username}</p>
+
+                        {user && user.id === response.user.id && (
+                            <div>
+                                <button onClick={() => {
+                                    const newContent = prompt('Edit response:', response.content);
+                                    if (newContent !== null) {
+                                        handleEdit(response.id, newContent);
+                                    }
+                                }}>Edit</button>
+                                <button onClick={() => {
+                                    if (window.confirm('Are you sure you want to delete this response?')) {
+                                        handleDelete(response.id);
+                                    }
+                                }}>Delete</button>
+                            </div>
+                        )}
                     </li>
                 ))}
             </ul>

433e8b5527845be78056c57d8bf765a4a868005b
luayidriss <idrissluay@gmail.com>
2023-10-24 12:42:41 +0000

    Add Alerts to Sign up and Sign In forms

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,54 +1,59 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Alert } from 'react-bootstrap';
 import axios from 'axios';
 import { useAuth } from './AuthContext';
 import { useHistory } from 'react-router-dom';
 
 function LoginForm() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-    const { login } = useAuth();
-    const history = useHistory();
+  const { login } = useAuth();
+  const history = useHistory();
 
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post('dj-rest-auth/login/', { username, password });
-            login();
-            console.log('Login successful:', response.data);
-            history.push('/')
-        } catch (error) {
-            console.error('Login error:', error);
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('dj-rest-auth/login/', { username, password });
+      login();
+      console.log('Login successful:', response.data);
+      history.push('/');
+    } catch (error) {
+      setError('Login failed. Please check your credentials.');
+      console.error('Login error:', error);
+    }
+  };
 
-    return (
-        <Form>
-            <Form.Group controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </Form.Group>
+  return (
+    <div>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <Form>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
 
-            <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Enter password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-            <Button variant="primary" onClick={handleLogin}>
-                Login
-            </Button>
-        </Form>
-    );
+        <Button variant="primary" onClick={handleLogin}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default LoginForm;
--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -1,79 +1,93 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Alert } from 'react-bootstrap';
 import axios from 'axios';
 import { useAuth } from './AuthContext';
 import { useHistory } from 'react-router-dom';
 
 function RegistrationForm() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password1, setPassword1] = useState('');
-    const [password2, setPassword2] = useState('');
-    const { login } = useAuth();
-    const history = useHistory();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [error, setError] = useState(null);
 
-    const handleRegister = async () => {
-        try {
-            await axios.post("dj-rest-auth/registration/", {
-                username,
-                email,
-                password1,
-                password2,
-            });
-            login();
-            history.push("/");
-        } catch (error) {
-            console.error('Sign Up error:', error);
-        }
-    };
+  const { login } = useAuth();
+  const history = useHistory();
 
-    return (
-        <Form>
-            <Form.Group controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </Form.Group>
+  const handleRegister = async () => {
+    try {
+      await axios.post('dj-rest-auth/registration/', {
+        username,
+        email,
+        password1,
+        password2,
+      });
+      login();
+      history.push('/');
+    } catch (error) {
+      // Check if there is a response from the server with specific error messages
+      if (error.response && error.response.data) {
+        const { username, email, password1, password2 } = error.response.data;
+        setError(
+          `${username || ''} ${email || ''} ${password1 || ''} ${password2 || ''}`
+        );
+      } else {
+        setError('Registration failed. Please check your information.');
+      }
+      console.error('Sign Up error:', error);
+    }
+  };
 
-            <Form.Group controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </Form.Group>
+  return (
+    <div>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <Form>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
 
-            <Form.Group controlId="password1">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Enter password"
-                    value={password1}
-                    onChange={(e) => setPassword1(e.target.value)}
-                />
-            </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-            <Form.Group controlId="password2">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Confirm password"
-                    value={password2}
-                    onChange={(e) => setPassword2(e.target.value)}
-                />
-            </Form.Group>
+        <Form.Group controlId="password1">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+        </Form.Group>
 
-            <Button variant="primary" onClick={handleRegister}>
-                Register
-            </Button>
-        </Form>
-    );
+        <Form.Group controlId="password2">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" onClick={handleRegister}>
+          Register
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default RegistrationForm;
